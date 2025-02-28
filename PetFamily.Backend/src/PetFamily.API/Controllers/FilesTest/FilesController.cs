06edc888f8ed49a8077f48ded92899481d6e69e6
NesterDP <s-dolg_96@mail.ru>
2025-02-28 18:05:13 +0300

    Revert "Revert "Id b 16""

--- PetFamily.Backend/src/PetFamily.API/Controllers/FilesTest/FilesController.cs
+++ PetFamily.Backend/src/PetFamily.API/Controllers/FilesTest/FilesController.cs
@@ -3,9 +3,8 @@ using Minio;
 using PetFamily.API.Controllers.FilesTest.Requests;
 using PetFamily.API.Extensions;
 using PetFamily.API.Processors;
-using PetFamily.Application.FilesManagement.Upload;
-using PetFamily.Application.FilesProvider.Delete;
-using PetFamily.Application.FilesProvider.Upload;
+using PetFamily.Application.Files.Delete;
+using PetFamily.Application.Files.Upload;
 
 namespace PetFamily.API.Controllers.FilesTest;
 
