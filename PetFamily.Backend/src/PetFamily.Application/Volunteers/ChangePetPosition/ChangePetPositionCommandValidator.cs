1c8dc30cf722d21b38369740c59e9e5634896a36
NesterDP <s-dolg_96@mail.ru>
2025-02-28 06:44:41 +0300

    Added mock test for UploadPhotosToPetHandler.

--- PetFamily.Backend/src/PetFamily.Application/Volunteers/ChangePetPosition/ChangePetPositionCommandValidator.cs
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/ChangePetPosition/ChangePetPositionCommandValidator.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using PetFamily.Application.Extensions;
 using PetFamily.Domain.PetContext.ValueObjects.PetVO;
+using PetFamily.Domain.Shared.CustomErrors;
 
 namespace PetFamily.Application.Volunteers.ChangePetPosition;
 
@@ -8,6 +9,8 @@ public class ChangePetPositionCommandValidator : AbstractValidator<ChangePetPosi
 {
     public ChangePetPositionCommandValidator()
     {
+        RuleFor(c => c.VolunteerId).NotEmpty().WithError(Errors.General.ValueIsRequired());
+        RuleFor(c => c.PetId).NotEmpty().WithError(Errors.General.ValueIsRequired());
         RuleFor(c => c.Position).MustBeValueObject(Position.Create);
     }
 }
