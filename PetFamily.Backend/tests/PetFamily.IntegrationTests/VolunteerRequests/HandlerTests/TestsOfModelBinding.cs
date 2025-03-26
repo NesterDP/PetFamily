7da81f2e39863d58d318c40fc3511c1df3709d5d
NesterDP <s-dolg_96@mail.ru>
2025-03-26 19:39:04 +0300

    Added an integration test that checks that all fields are actually bound to the database.

--- PetFamily.Backend/tests/PetFamily.IntegrationTests/VolunteerRequests/HandlerTests/TestsOfModelBinding.cs
+++ PetFamily.Backend/tests/PetFamily.IntegrationTests/VolunteerRequests/HandlerTests/TestsOfModelBinding.cs
@@ -2,6 +2,9 @@ using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using PetFamily.IntegrationTests.General;
 using PetFamily.IntegrationTests.VolunteerRequests.Heritage;
+using PetFamily.SharedKernel.ValueObjects.Ids;
+using PetFamily.VolunteerRequests.Domain.Entities;
+using PetFamily.VolunteerRequests.Domain.ValueObjects;
 
 namespace PetFamily.IntegrationTests.VolunteerRequests.HandlerTests;
 
@@ -13,20 +16,47 @@ public class TestsOfModelBinding : VolunteerRequestsTestsBase
     }
 
     [Fact]
-    public async Task SoftDeletePet_success_should_delete_2_pets_that_were_soft_deleted_with_expired_lifetime()
+    public async Task Test_should_add_to_2_requests_to_database()
     {
         // arrange
-        var DEFAULT_TEXT = "default text";
-
+        
+        
         // act
         var result_1 = await DataGenerator.SeedVolunteerRequest(WriteDbContext);
         var result_2 = await DataGenerator.SeedVolunteerRequest(WriteDbContext);
 
         // assert
         WriteDbContext.VolunteerRequests.Count().Should().Be(2);
-        var request = await WriteDbContext.VolunteerRequests.FirstOrDefaultAsync();
-        request.Should().NotBeNull();
-        request.AdminId.Should().BeNull();
-        request.VolunteerInfo.Value.Should().Be(DEFAULT_TEXT);
+    }
+    
+    [Fact]
+    public async Task Test_should_add_request_to_database_with_all_its_fields_being_non_null()
+    {
+        // Arrange
+        var DEFAULT_TEXT = "default text";
+        var REJECTION_TEXT = "rejection text";
+        var userId = UserId.NewUserId();
+        var volunteerInfo = VolunteerInfo.Create(DEFAULT_TEXT).Value;
+        var request = new VolunteerRequest(userId, volunteerInfo);
+        
+        var adminId = AdminId.NewAdminId();
+        var rejectionComment = RejectionComment.Create(REJECTION_TEXT).Value;
+        request.SetRevisionRequired(adminId, rejectionComment);
+        
+
+        // act
+        WriteDbContext.VolunteerRequests.Add(request);
+        await WriteDbContext.SaveChangesAsync();
+        
+        // assert
+        var result = await WriteDbContext.VolunteerRequests.FirstOrDefaultAsync();
+        result.Id.Value.Should().Be(request.Id);
+        result.AdminId.Should().Be(adminId);
+        result.UserId.Should().Be(userId);
+        result.VolunteerInfo.Value.Should().Be(DEFAULT_TEXT);
+        result.Status.Value.Should().Be(VolunteerRequestStatusEnum.RevisionRequired);
+        result.CreatedAt.Should().BeBefore(DateTime.UtcNow);
+        result.CreatedAt.Should().BeAfter(DateTime.UtcNow.AddHours(-1));
+        result.RejectionComment.Value.Should().Be(rejectionComment.Value);
     }
 }
