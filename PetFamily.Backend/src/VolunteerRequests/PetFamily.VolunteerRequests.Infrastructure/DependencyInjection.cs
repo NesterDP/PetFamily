a2494c15d6a80fa35594324744143060e2619bfb
NesterDP <s-dolg_96@mail.ru>
2025-03-26 12:40:28 +0300

    Added prototype of module "VolunteerRequests" (empty projects with configured dependencies).

---
+++ PetFamily.Backend/src/VolunteerRequests/PetFamily.VolunteerRequests.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PetFamily.VolunteerRequests.Infrastructure;
+
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddVolunteerRequestsInfrastructure(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
---
+++ PetFamily.Backend/src/VolunteerRequests/PetFamily.VolunteerRequests.Presentation/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PetFamily.VolunteerRequests.Presentation;
+
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddVolunteerRequestsContracts(this IServiceCollection services)
+    {
+        return services;
+    }
+}
