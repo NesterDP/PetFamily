8402a2b1df5b04ddb6cf207ce4ff45177fbc8a42
NesterDP <s-dolg_96@mail.ru>
2025-04-07 05:10:07 +0300

    Packed backend application into folder in git repository

---
+++ PetFamily.Backend/Backend/src/VolunteerRequests/PetFamily.VolunteerRequests.Infrastructure/Migrations/20250330171525_VolunteerRequests_Init.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.VolunteerRequests.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class VolunteerRequests_Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "volunteer_requests");
+
+            migrationBuilder.CreateTable(
+                name: "volunteer_requests",
+                schema: "volunteer_requests",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    admin_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    revision_comment = table.Column<string>(type: "text", nullable: true),
+                    rejected_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    volunteer_info = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_volunteer_requests", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "volunteer_requests",
+                schema: "volunteer_requests");
+        }
+    }
+}
--- PetFamily.Backend/FileService/src/FileService/Program.cs
+++ PetFamily.Backend/FileService/src/FileService/Program.cs
@@ -14,5 +14,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.Run();
